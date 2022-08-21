f5b75412694d1c7e2338553bd7ac44f4918a68cd
deepak <its.deepaklodhi@gmail.com>
2022-08-21 14:52:53 +0530

    first commit

---
+++ assets/js/business-profile.js
@@ -0,0 +1,5 @@
+function openform(){
+    
+    document.getElementById("upload-form").style.display="block";
+    
+};
---
+++ assets/js/header.js
@@ -0,0 +1,13 @@
+function opensearch(){
+    document.getElementById("nav-container").style.display="none";
+    document.getElementById("search-bar").style.display="flex";
+    
+};
+
+function closesearch(){
+
+      document.getElementById("search-bar").style.display="none";
+    
+    document.getElementById("nav-container").style.display="block";
+  
+};
